Bug fix. Rewritten unit conversion using astropy units.

CloudyGalaxy/gas_stats.py
@@ -49,8 +49,8 @@ class GasStatsCL01:
         :param spec: Luminosity values of spectrum bins in erg*s^-1*AA^-1
         :return: Rate of ionizing photons in s^-1
         '''
-        h = (const.h * u.erg * u.s**-1).value # erg * s^-1
-        c = (const.c * cds.AA * u.s**-1).value # AA * s^-1
+        h = const.h.to(u.erg * u.s**-1).value # erg * s^-1
+        c = const.c.to(cds.AA * u.s**-1).value # AA * s^-1
 
         mask = lambda_ <= 912.
         integral = simps(lambda_[mask] * spec[mask], lambda_[mask])
@@ -77,7 +77,7 @@ class GasStatsCL01:
         :param nH: Hydrogen density in cm^-3
         :return: Filling factor
         '''
-        c = (const.c * u.cm * u.s**-1).value # cm * s^-1
+        c = const.c.to(u.cm * u.s**-1).value # cm * s^-1
         alpha_B = 2.59e-13  # cm^3 s^-1  Alpha Case B (not in astropy units)
         return ((4 / 3) * np.pi * c ** 3 / alpha_B ** 2 * U_0 ** 3 / (Q_0 * nH)) ** 0.5
 
@@ -150,8 +150,8 @@ class GasStatsBy17:
         :param spec: Luminosity values of spectrum bins in erg*s^-1*AA^-1
         :return: Rate of ionizing photons
         '''
-        h = (const.h * u.erg * u.s ** -1).value  # erg * s^-1
-        c = (const.c * cds.AA * u.s ** -1).value  # AA * s^-1
+        h = const.h.to(u.erg * u.s ** -1).value  # erg * s^-1
+        c = const.c.to(cds.AA * u.s ** -1).value  # AA * s^-1
 
         mask = lambda_ <= 912.
         integrand = lambda_[mask] * spec[mask]
@@ -163,7 +163,7 @@ class GasStatsBy17:
         Calculate rate of ionizing photons necessary to satisfy conditions at zero-age.
         :return: Rate of ionizing photons
         '''
-        c = (const.c * u.cm * u.s**-1).value # cm * s^-1
+        c = const.c.to(u.cm * u.s**-1).value # cm * s^-1
         self.Q_0 = 10 ** self.logU * 4 * np.pi * self.r_inner ** 2 * self.nH * c
         return self.Q_0
 