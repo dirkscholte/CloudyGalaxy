Fix bug Astropy Angstrom units imported from cds submodule.

CloudyGalaxy/gas_stats.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.integrate import simps
 from astropy import constants as const
 from astropy import units as u
+from astropy.units import cds
 
 class GasStatsCL01:
     '''
@@ -49,7 +50,7 @@ class GasStatsCL01:
         :return: Rate of ionizing photons in s^-1
         '''
         h = (const.h * u.erg * u.s**-1).value # erg * s^-1
-        c = (const.c * u.cds.AA * u.s**-1).value # AA * s^-1
+        c = (const.c * cds.AA * u.s**-1).value # AA * s^-1
 
         mask = lambda_ <= 912.
         integral = simps(lambda_[mask] * spec[mask], lambda_[mask])
@@ -150,7 +151,7 @@ class GasStatsBy17:
         :return: Rate of ionizing photons
         '''
         h = (const.h * u.erg * u.s ** -1).value  # erg * s^-1
-        c = (const.c * u.cds.AA * u.s ** -1).value  # AA * s^-1
+        c = (const.c * cds.AA * u.s ** -1).value  # AA * s^-1
 
         mask = lambda_ <= 912.
         integrand = lambda_[mask] * spec[mask]