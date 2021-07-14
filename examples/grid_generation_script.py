61919ebd8a8df9916ddbe2a4c7b8d2eee9ea4735
Dirk Scholte <dirk.scholte.20@ucl.ac.uk>
2021-07-14 10:57:07 +0100

    Add example script to generate a grid of Cloudy models and write emission lines to file.

---
+++ examples/grid_generation_script.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pyCloudy as pc
+from CloudyGalaxy.gas_stats import GasStatsBy17 as GasStats
+from CloudyGalaxy.abundances import ElementalAbundances, convert_xi_to_F
+from CloudyGalaxy.read_write_cloudy import StellarSpectrum, format_emission_table, make_input_file, make_emission_line_files
+
+n_proc              = 6
+
+output_dir          = './output_data/'
+model_name          = 'test_model_high_res_age_2Myr'
+data_path           = '../input_data/'
+star_table_filename = 'Chabrier_constant_SFH_IMF_lower_0p08_IMF_upper_120_star_table.ASCII'
+emission_line_list  = format_emission_table('../input_data/LineList_HII.dat')
+
+nlogZs = 24
+nlogUs = 33
+nxis   = 9
+ntaus  = 24
+
+logZs = np.linspace(-1.0, 0.7, nlogZs)
+logUs = np.linspace(-4.0, -1.0, nlogUs)
+xis   = np.linspace(0.1, 0.6, nxis)
+taus  = np.linspace(0.01, 4.0, ntaus)
+Fs    = np.ones((nlogZs,nxis))*np.nan
+
+pc.print_make_file(dir_ = output_dir)
+for i in range(nlogZs):
+    logZ = logZs[i]
+    xi_to_F = convert_xi_to_F(logZ)
+    for j in range(nlogUs):
+        for k in range(nxis):
+            logU = logUs[j]
+            xi = xis[k]
+            F = xi_to_F(xi)
+            Fs[i,k] = F
+            stellar_spectrum = StellarSpectrum(data_path, star_table_filename)
+            gas_stats = GasStats(stellar_spectrum, logZ, logU, r_inner=1e19, age=2e6)
+            gas_stats.calc_all_parameters()
+            elemental_abundances = ElementalAbundances(logZ, F)
+            elemental_abundances.calc_all_parameters()
+            make_input_file(output_dir, model_name, logZ, logU, xi, emission_line_list, stellar_spectrum, gas_stats, elemental_abundances, age=2e6)
+
+pc.run_cloudy(dir_ = output_dir, n_proc = n_proc, model_name = model_name, use_make = True)
+
+make_emission_line_files(output_dir, model_name, logZs, logUs, xis, taus, Fs)
